7404e8d2b8a49a2a509bc2da41af044e3170eb9e
ganyar najjar <najjar.gane@gmail.com>
2014-07-10 14:19:45 +0300

    na eshe taya

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+$(document).ready(function(){
+	
+	
+	
+	$("#first-menu").click(
+		function (){
+			$(".content").hide();
+			$("#first-content").show();
+			
+		}
+	);
+	
+	$("#second-menu").click(
+		function (){
+			$(".content").hide();
+			$("#second-content").show();
+		}
+	);
+	
+	$("#third-menu").click(
+		function (){
+			$(".content").hide();
+			$("#third-content").show();
+		}
+	);
+	
+	
+});
