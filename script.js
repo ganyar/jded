218842360e901357fd30aa222c51f7c1d03491f2
ganyar najjar <najjar.gane@gmail.com>
2014-07-11 21:01:19 +0300

    added ralway font added velocity effects added chenged color

--- script.js
+++ script.js
@@ -8,23 +8,35 @@ $(document).ready(function(){
 	
 	$("#first-menu").click(
 		function (){
-			$(".content").hide();
-			$("#first-content").show();
 			
+			$(".content").hide();
+			//$("#first-content").show();
+			
+			$(".menu-item").removeClass("menu-item-selected");
+			$("#first-menu").addClass("menu-item-selected");
+			
+			$("#first-content").velocity("transition.slideRightBigIn", { stagger: 200 });
 		}
 	);
 	
 	$("#second-menu").click(
 		function (){
 			$(".content").hide();
-			$("#second-content").show();
+			//$("#second-content").show();
+			$("#second-content").velocity("transition.slideRightBigIn", { stagger: 200 });
+			$(".menu-item").removeClass("menu-item-selected");
+			$("#second-menu").addClass("menu-item-selected");
 		}
 	);
 	
 	$("#third-menu").click(
 		function (){
 			$(".content").hide();
-			$("#third-content").show();
+			//$("#third-content").show();
+			$("#third-content").velocity("transition.slideRightBigIn", { stagger: 200 });
+			$(".menu-item").removeClass("menu-item-selected");			
+			$("#third-menu").addClass("menu-item-selected");
+
 		}
 	);
 	
